849b2f37cc639bc75acbcea8e68ea1b0f1e5ccc0
Tony <tony@DESKTOP-C5TKN10.localdomain>
2023-07-29 14:22:16 +1000

    Added basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Password';
+  userInput:number = 0;
+  answerOutput: string = '';
+  includeNumbers = false;
+;
+  onKey(event: any){
+    let input = event.target.value
+    this.userInput = event.target.value
+  }
+
+  numberChange(){
+  }
+
+  onClick(length: number){
+    this.answerOutput = '';
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = 1234567890;
+    const symbols = '!@#$%';
+    const charactersLength = characters.length;
+      for ( let i = 0; i < length; i++ ) {
+          this.answerOutput += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+    console.log(this.answerOutput)
+  }
 }
