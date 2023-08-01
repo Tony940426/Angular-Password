1d66dd2d83525485e70410181436b0a282c85dc2
Tony <tony@DESKTOP-C5TKN10.localdomain>
2023-08-01 20:24:11 +1000

    Implemented logic to push user password selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'Angular-Password';
   userInput:number = 0;
   answerOutput: string = '';
+  pool:string = '';
   includeNumbers = false;
 ;
   onKey(event: any){
@@ -16,18 +17,35 @@ export class AppComponent {
     this.userInput = event.target.value
   }
 
-  numberChange(){
+  letterChecked(event: any){
+    let letters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      if (event.target.checked == true) {
+        this.pool += letters
+      }
+    }
+
+  numberChecked(event: any){
+  let numbers ='0123456789'
+    if (event.target.checked == true) {
+      this.pool += numbers
+    }
   }
+
+  symbolChecked(event: any){
+    let symbols ='!@#$%'
+      if (event.target.checked == true) {
+        this.pool += symbols;
+      }
+    }
+
 
   onClick(length: number){
     this.answerOutput = '';
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = 1234567890;
-    const symbols = '!@#$%';
-    const charactersLength = characters.length;
-      for ( let i = 0; i < length; i++ ) {
-          this.answerOutput += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-    console.log(this.answerOutput)
+    const charactersLength = this.pool.length;
+
+    for ( let i = 0; i < length; i++ ) {
+        this.answerOutput += this.pool.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(this.pool)
   }
 }
